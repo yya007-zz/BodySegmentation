036a061c42f703bbd3bc427e059f23ebc9c0f594
yaoy4 <yaoy4@miccai.multi.atlas@gmail.com>
2017-04-07 00:19:33 -0500

    3

--- test_bd.py
+++ test_bd.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy import misc
 import scipy.io as sio
-
+from time import time
 
 class dataFetch(object):
     
@@ -71,7 +71,7 @@ class dataFetch(object):
     
     
     
-    
+''' 
 mydataFetch=dataFetch()
 dataset='train'
 subset='seg'    
@@ -92,7 +92,7 @@ print np.sum(image[100]-seg[:,100,:])
 print np.sum(image-seg.transpose(1,0,2))
 
 
-'''
+
 
 objectInd=1
 accuracy1=0.00001
