6673dd96610bc0a7ad3059e69f852dd0842d58ba
yaoy4 <yaoy4@miccai.multi.atlas@gmail.com>
2017-04-10 16:54:53 -0500

    4

--- evaluate.py
+++ evaluate.py
@@ -179,6 +179,7 @@ if gap<3:
     gap=3
 
 mydataFetch=dataFetch()
+'''
 #Network structure--------------------------                                 
 sess = tf.InteractiveSession()
 x = tf.placeholder(tf.float32, shape=[None,512,512,3])
@@ -193,7 +194,7 @@ correct_prediction = tf.equal(tf.argmax(y_conv,3), tf.argmax(y_,3))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 result =tf.argmax(y_conv,3)
 sess.run(tf.global_variables_initializer())
-
+'''
 
 
 metadir=
@@ -229,8 +230,7 @@ randomstate='random'
 modeldir=('../network/model_%d_%s'%(epoch,randomstate))
 with tf.Session() as sess:
   print "start load network"
-  new_saver = tf.train.import_meta_graph(modeldir+'.meta',
-      clear_devices=True)
+  new_saver = tf.train.import_meta_graph(modeldir+'.meta',clear_devices=True)
   new_saver.restore(sess, modeldir)
 
 
