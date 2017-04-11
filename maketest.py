5c651fa020e984a12387427d6ef7439b16efe738
yaoy4 <yaoy4@miccai.multi.atlas@gmail.com>
2017-04-11 16:58:59 -0500

    123

---
+++ maketest.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+import FCN1
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import time
+import sys
+from scipy.misc import imsave
+from test_bd import *
+def load_image( infilename ) :
+    img = Image.open( infilename )
+    img = img.resize((512, 512), Image.NEAREST)      # use nearest neighbour
+    #img = img.resize((width, height), Image.BILINEAR)     # linear interpolation in a 2x2 environment
+    #img = img.resize((width, height), Image.BICUBIC)      # cubic spline interpolation in a 4x4 environment
+    #img = img.resize((width, height), Image.ANTIALIAS)    # best down-sizing filter
+    img.load()
+    data = np.asarray( img, dtype="int32" )
+    return data
+
+def save_image( npdata, outfilename ) :
+    img = Image.fromarray( np.asarray( np.clip(npdata,0,255), dtype="uint8"), "L" )
+    img.save( outfilename )
+
+def next_batch(pos,size,data):
+  if pos+size<data.shape[0]:
+    return pos+size,data[pos:pos+size]
+  else:
+    return pos+size-data.shape[0],np.concatenate((data[pos:],data[0:pos+size-data.shape[0]]),axis=0)
+    
+def prepareY(y,number_of_classes):
+    yf=y.flatten()
+    #print yf.shape
+    #print y.shape
+    res=np.zeros([yf.shape[0],number_of_classes])
+    for i in range(number_of_classes):
+        res[yf[:]==i,i]=1
+    return res.reshape([y.shape[0],y.shape[1],y.shape[2],number_of_classes])
+    
+    
+#0-255 2d gray image
+def prepareX(gray):
+    #gray=gray.astype(int)
+    #gray=gray.astype(float)
+    VGG_MEAN = [103.939, 116.779, 123.68]
+    res=np.zeros([gray.shape[0],512,512,3])
+    res[:,:,:,2]= gray-VGG_MEAN[2]
+    res[:,:,:,1]= gray-VGG_MEAN[1]
+    res[:,:,:,0]= gray-VGG_MEAN[0]    
+    return res 
+objectNum=75
+viewNum=3
+size=16
+if True:
+    selectorder=np.arange(0,objectNum*viewNum*512,viewNum*512)
+    selectorder=selectorder+2*512+256
+    pos=0
+    for k in range(len(selectorder)/size):
+        assert k==0
+        pos,sample=next_batch(pos,size,selectorder)       
+        imgs=mydataFetch.getdata(sample,'test','img')
+        segs=mydataFetch.getdata(sample,'test','seg')
+        imgs=prepareX(imgs)
+        segs=prepareY(segs,number_of_classes)
+        np.save('../bigfile/testimgs.npy',imgs)
+        np.save('../bigfile/testsegs.npy',segs)
