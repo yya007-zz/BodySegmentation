66b4e312df1b0cb566f3096fd605b4ce97132d6f
yaoy4 <yaoy4@miccai.multi.atlas@gmail.com>
2017-04-20 15:05:59 -0500

    123

--- run.py
+++ run.py
@@ -90,40 +90,42 @@ def trainEpoch(evaluate=False,restore=True,save=True):
             epochind+=1
             modelname=('model_%s_%d'%(state,epochind))
             modelfolddir=('../network/'+modelname)
+        
         if  epochind>0:
             print ('start loading model_%s_%d'%(state,epochind-1)) 
             modelname=('model_%s_%d'%(state,epochind-1))           
             modeldir=('../network/%s/%s'%(modelname,modelname))
             saver.restore(sess,modeldir)
-            if evaluate:
-                print "start evaluation"
-                resdir='../res/%s_%d/'%(state,epochind-1)
-                testall(sess,result,number_of_classes,x,y_,keep_prob,quicktest=quicktest,resdir=resdir,saveres=True)
-            
-        modelname=('model_%s_%d'%(state,epochind))
-        print "need new model",modelname
-        for iterind in range(iterationsOne):
-            pos=0
-            #if not quicktest:
-            if True:
-                pos,sample=next_batch(pos,size,selectorder)    
-                imgs=mydataFetch.getdata(sample,'train','img')
-                segs=mydataFetch.getdata(sample,'train','seg')
-                imgs=prepareX(imgs)
-                segs=prepareY(segs,number_of_classes)
-            train_step.run(feed_dict={x: imgs, y_: segs, keep_prob: 0.5}) 
-            if iterind%gap == 0 or iterind==iterationsOne-1:
-                cp=correct_prediction.eval(feed_dict={x: imgs, y_: segs,keep_prob: 1.0})
-                ce=cross_entropy.eval(feed_dict={x: imgs, y_: segs,keep_prob: 1.0})
-                ac=np.mean(cp)
-                ac2=np.mean(cp[1:])
-                print("epoch: %d,step: %d, training accuracy %.4f, only label: %.4f, loss %g, time %d"%(epochind,iterind, ac,ac2,ce,time()-t0))
-                t0 = time()
+        
+        if evaluate:
+            print "start evaluation"
+            resdir='../res/%s_%d/'%(state,epochind-1)
+            testall(sess,result,number_of_classes,x,y_,keep_prob,quicktest=quicktest,resdir=resdir,saveres=True)
+        else:    
+            modelname=('model_%s_%d'%(state,epochind))
+            print "need new model",modelname
+            for iterind in range(iterationsOne):
+                pos=0
+                #if not quicktest:
+                if True:
+                    pos,sample=next_batch(pos,size,selectorder)    
+                    imgs=mydataFetch.getdata(sample,'train','img')
+                    segs=mydataFetch.getdata(sample,'train','seg')
+                    imgs=prepareX(imgs)
+                    segs=prepareY(segs,number_of_classes)
+                train_step.run(feed_dict={x: imgs, y_: segs, keep_prob: 0.5}) 
+                if iterind%gap == 0 or iterind==iterationsOne-1:
+                    cp=correct_prediction.eval(feed_dict={x: imgs, y_: segs,keep_prob: 1.0})
+                    ce=cross_entropy.eval(feed_dict={x: imgs, y_: segs,keep_prob: 1.0})
+                    ac=np.mean(cp)
+                    ac2=np.mean(cp[1:])
+                    print("epoch: %d,step: %d, training accuracy %.4f, only label: %.4f, loss %g, time %d"%(epochind,iterind, ac,ac2,ce,time()-t0))
+                    t0 = time()
 
-        if save:
-            savemodel(modelname,saver,sess)
-            epochind=epochind+1
-            print "successfully save model"
+            if save:
+                savemodel(modelname,saver,sess)
+                epochind=epochind+1
+                print "successfully save model"
             
 #trainEpoch()
 trainEpoch(evaluate=True)
