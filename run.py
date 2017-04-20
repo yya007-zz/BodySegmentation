c395da38211e7f3f1056eb318da076996712a8da
yaoy4 <yaoy4@miccai.multi.atlas@gmail.com>
2017-04-19 22:29:57 -0500

    123

--- run.py
+++ run.py
@@ -84,8 +84,6 @@ with tf.Session() as sess:
     print "----------------start training"
     print "traindata: %d state: %s, epoch,iterations per epoch: %d,%d, gap: %d "%(len(selectorder),state,epoch,iterationsOne,gap)
     t0 = time()
-    
-    '''   
     sess.run(init)
     if quicktest:
         imgs=np.load('../bigfile/testimgs.npy')
@@ -127,7 +125,6 @@ with tf.Session() as sess:
             print "start evaluation"
             resdir='../res/%s_%d_%d/'%(state,epoch,epochind)
             testall(sess,result,number_of_classes,x,y_,keep_prob,quicktest=quicktest,resdir=resdir,saveres=True)
-'''
 print "finished"
 
 
